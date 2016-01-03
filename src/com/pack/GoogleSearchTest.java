37823ce470137a3126b5cdd404472e036a2ce18c
COD <COD@COD-PC>
2016-01-03 16:57:15 +0530

    First

---
+++ src/com/pack/GoogleSearchTest.java
@@ -0,0 +1,17 @@
+package com.pack;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class GoogleSearchTest {
+	
+	public static void main(String...args) {
+		
+		WebDriver driver  = new FirefoxDriver();
+		driver.navigate().to("http://google.com");
+		String appTitle = driver.getTitle();
+		System.out.println("Application title is :: "+appTitle);
+		driver.quit();
+	}
+
+}
